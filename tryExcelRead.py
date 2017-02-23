3eab65459d36bf7f8213f14233168c56f0bdb68e
SJM541 <MarshallS@cabi.org>
2017-02-23 16:53:43 +0000

    Endofday
    not yet working fully

--- tryExcelRead.py
+++ tryExcelRead.py
@@ -4,4 +4,38 @@
 
 import pandas as pd
 
-df = pd.read_excel(open('C:/Users/marshalls/Documents/SJM/RemoteDiagnostics/ContextModel/Remote_Diagnostics_Data.xlsx','rb'),sheetname='CPC_crop-host_model_data')
+
+def main():
+
+
+    countryName='UK'
+    cropName = 'Cabbage'
+
+    print ('working')
+
+    fileName = 'C:/Users/marshalls/Documents/SJM/RemoteDiagnostics/ContextModel/Remote_Diagnostics_Data.xlsx'
+
+    Locations = pd.read_excel(open(fileName,'rb'),sheetname='CPC_pest_location_model_data')
+    Hosts = pd.read_excel(open(fileName,'rb'),sheetname='CPC_crop-host_model_data')
+
+    print('Locations \n')
+    print(Locations)
+    print('\n Hosts \n')
+    print(Hosts)
+
+    countrySelection=[]
+    
+    
+    for loc in Locations['Country']:
+        print(loc)
+        if (loc == countryName):
+            countrySelection.append(Locations['Scientific name']) # Hasn't appended anything!
+    print(countrySelection)
+
+    # do same for crops
+    # merge the countrySelection and cropSelection dfs based on common scientific names
+    
+    
+    
+if __name__ == "__main__":
+    main()
